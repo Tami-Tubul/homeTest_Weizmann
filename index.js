c4fa59176cc00d06222261143383737ddfbace5b
Tami-Tubul <tamit0430@gmail.com>
2023-01-02 20:03:57 +0200

    first commit

--- index.js
+++ index.js
@@ -43,10 +43,32 @@ window.onclick = function (event) {
 
 
 
+//******************animated articles***************/
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+
 //*************mobile menu******************/
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
-toggleButton.addEventListener("click" , () => {
+toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("open")
 })
